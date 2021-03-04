b82ebf37e3a2b8d95231ea5545724c74dc5382ae
Delal Tomruk <66208179+delaltomruk@users.noreply.github.com>
2021-03-04 22:26:27 +0300

    Delete twitter_scraper.py

--- scripts/twitter_scraper.py
+++
@@ -1,3 +0,0 @@
-from twitter_hashtag_scraper import TwitterHashtagScraper
-
-TwitterHashtagScraper(hashtag="#MalagaCF",x_guest_token="1331317358300979202",use_proxy=False,output_path="/Users/youruser/Desktop",max_tweets=60).collect()
